f3caf8f506a95a39a403b4436d4154b477a6f26f
chris284891 <chris284891@gmail.com>
2020-03-05 11:48:43 -0500

    added delete button

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 
 import { Hero }         from '../hero';
 import { HeroService }  from '../hero.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hero-detail',
